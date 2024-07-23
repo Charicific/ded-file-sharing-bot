5a9cf4dbb8396569be1cc31283b692cfae98d669
Charicific <79819892+Charicific@users.noreply.github.com>
2024-07-23 21:16:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002161598191"))
 
 CHANNEL_1_ID = '-1002029221637'
 CHANNEL_2_ID = '-1002161598191'
