a75598819ca3846221b9e75b8dcb4ac90852c7c9
Charicific <79819892+Charicific@users.noreply.github.com>
2024-07-24 12:28:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ import time
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserNotParticipant
 
 from bot import Bot
 from config import (
